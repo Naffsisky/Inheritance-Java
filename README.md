d12080326353c93c5a567e7bd6840082ae5a63ef
naffsvn <naffsvn@protonmail.com>
2023-05-13 21:22:21 +0700

    v1.1

---
+++ README.md
@@ -0,0 +1,12 @@
+## Output
+
+### V1
+
+```
+NPM		: 21081010278
+Judul		: Java
+Penerbit	: Erlangga
+Tahun		: 2022
+Penulis		: Prinafsika
+Edisi		: Lengkap
+```
--- src/main/java/com/mycompany/inheritance/Inheritance.java
+++ src/main/java/com/mycompany/inheritance/Inheritance.java
@@ -12,9 +12,9 @@ public class Inheritance {
 
     public static void main(String[] args) {
         Koleksi buku = new Koleksi("Java", "Erlangga", 2022);
-        Buku penulis = new Buku("C++", "Aku", "Erlangga", 2023 );
+        Buku penulis = new Buku("C++", "Prinafsika", "Erlangga", 2023 );
         Majalah edisi = new Majalah("Python", "Lengkap", "Erlangga", 2024);
-        Skripsi npm = new Skripsi("21081010278", "Aku", "C#", "Erlangga", 2025);
+        Skripsi npm = new Skripsi("21081010278", "Prinafsika", "C#", "Erlangga", 2025);
         npm.output();
         buku.output();
         penulis.output();
